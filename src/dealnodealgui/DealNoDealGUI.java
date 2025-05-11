cc15b96952975628386499b37d27f8e51d0a718c
Benjamin Macpherson <ben.macpherson.2005@gmail.com>
2025-05-12 10:08:31 +1200

    Innit Comitt

---
+++ src/dealnodealgui/DealNoDealGUI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dealnodealgui;
+
+/**
+ *
+ * @author benma
+ */
+public class DealNoDealGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello I am howie mandel give me deal or no deal");
+    }
+    
+}
