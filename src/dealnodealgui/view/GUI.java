e60708f8abeb075d67d9538c9de2f4de894c1f8b
Benjamin Macpherson <bm19439@my.westlake.school.nz>
2025-05-12 11:51:36 +1200

    Set up file structure to follow MVC pattern

---
+++ src/dealnodealgui/view/GUI.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dealnodealgui.view;
+import java.swing.*;
+
+/**
+ *
+ * @author benma
+ */
+
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    private JFrame gameWindow;
+    private JPanel Box;
+   
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
