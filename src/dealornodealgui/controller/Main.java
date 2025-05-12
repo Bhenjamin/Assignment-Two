7da18e51e3bc42414303bed22cf8ad8785b48250
Benjamin Macpherson <bm19439@my.westlake.school.nz>
2025-05-12 12:17:01 +1200

    Set up file structure to follow MVC pattern fixed

---
+++ src/dealornodealgui/controller/Main.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dealnodealgui.controller;
+
+/**
+ *
+ * @author benma
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    // Initialising variables
+    public static ArrayList<Box> boxList = new ArrayList<>();
+    public static Random rand = new Random();
+    public static Player player = new Player();
+    public static Scores scores = new Scores();
+    public static Scanner scan = new Scanner(System.in);
+    public static LinkedHashMap<String, Integer> playerInfo = new LinkedHashMap<>();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Boxes.initialiseBoxes();
+        
+        playerInfo = scores.getScores();
+        scores.printTopScores();
+        
+        System.out.println("Hello, Welcome to Deal or No Deal!");
+        player.setName(GameLogic.initialisePlayer());
+        
+        Boxes.displayPlayerBox();
+        Boxes.displayBoxes();
+        GameLogic.gameLogic();
+
+        scan.close();
+    }
+    
+}
